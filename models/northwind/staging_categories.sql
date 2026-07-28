162e5e666fd38dbc1b75503d1bcdd9e30b8e4d83
GabinLandry <mbopdagabin@gmail.com>
2026-07-28 17:44:19 +0200

    fix

--- models/northwind/staging_categories.sql
+++ models/northwind/staging_categories.sql
@@ -1,10 +1,10 @@
 WITH source_data AS (
     SELECT *
-    FROM {{ source('northwind', 'categories') }}
+    FROM {{ source('northwind_data', 'categories') }}
 )
 SELECT 
 	categoryid AS category_id
 	,categoryname AS category_name
---	,description
---	,picture
+	,description
+	,picture
 FROM source_data
